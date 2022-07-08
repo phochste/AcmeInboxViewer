cbfaed6f42e1603849af4c9bbc9a49024bf4db60
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2022-07-08 06:43:34 +0200

    Adding support for deletion via checkboxes

---
+++ src/Fields/Checkbox.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { createEventDispatcher } from 'svelte';
+
+    export let checked : boolean = false;
+
+    const dispatch = createEventDispatcher();
+
+    function clicked() {
+        checked = ! checked;
+        dispatch('checked', { checked: checked});
+    }
+</script>
+
+<button on:click|stopPropagation={clicked}>
+{#if checked}✓{:else}&nbsp;{/if}
+</button>
+
+<style>
+    button {
+        width: 30px;
+        height: 30px;
+    }
+</style>
--- src/InboxList.svelte
+++ src/InboxList.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import Checkbox from './Fields/Checkbox.svelte';
     import { deleteInboxItem, loadInbox, prettyUris, type MessageInfo } from './inbox';
     import { createEventDispatcher } from 'svelte';
     import { watchContainer } from './container';
@@ -13,6 +14,8 @@
     export let maxItems : number = 50;
 
     let inboxResources : MessageInfo[] = [];
+    let checkAll : boolean = false;
+    let checkedMail : Set<MessageInfo> = new Set<MessageInfo>();
         
     listAll(inbox);
 
@@ -32,18 +35,52 @@
     function handleNew() {
         dispatch('new', {});
     }
+
+    function handleCheck(e,mail) {
+        if (e.detail.checked) {
+            checkedMail.add(mail);
+        }
+        else {
+            checkedMail.delete(mail);
+        }
+        checkedMail = new Set(checkedMail);
+        console.log(checkedMail)
+    }
+
+    function handleDeleteChecked() {
+        checkedMail.forEach( (mail) => {
+            deleteMail(mail);
+        });
+        checkedMail = new Set<MessageInfo>();
+        checkAll = false;
+    }
+
+    function handleSelectAll(e) {
+        checkedMail = new Set<MessageInfo>(); 
+        if (e.detail.checked) {
+            inboxResources.forEach( (mail) => {
+                 checkedMail.add(mail);
+            });
+        }
+    }
 </script>
 
 <svelte:head>
     <title>Inbox ({inboxResources.length}) - {inbox}</title>
 </svelte:head>
 
-<button  class="btn btn-primary" on:click={handleNew}>New message</button>
+<button class="btn btn-primary" on:click={handleNew}>New message</button>
 <hr/>
 <button class="btn btn-info" on:click={() => listAll(inbox) }>🔃 {inbox} ({inboxResources.length})</button>
 <hr/>
 
 <h4>Messages</h4>
+<div class="list-menu">
+     <Checkbox bind:checked={checkAll} on:checked={handleSelectAll}/>
+{#if checkedMail.size}
+     <button class="delete" on:click={handleDeleteChecked}>🗑</button>
+{/if}
+</div>
 {#if inboxResources.length}
 <table class="table table-hover">
     <thead>
@@ -60,30 +97,52 @@
     {:then activity} 
         <tr on:click={ () => { selected = mail.resource.url } } >
             {#if activity}
+                <td>
+                    <Checkbox 
+                        checked={checkedMail.has(mail)}    
+                        on:checked={(e) => handleCheck(e,mail)}/>
+                </td>
                 <td><b>{activity.actor.name ? activity.actor.name : 'Unknown' }</b></td>
                 <td>{prettyUris(activity.types,", ")}</td>
                 <td>{prettyUris(activity.object.types,", ")}</td>
                 <td>{mail.resource.modified.toISOString()}</td>
-                <td><button on:click|stopPropagation={ () => deleteMail(mail)}>🗑</button></td>
+                <td><button class="delete" on:click|stopPropagation={ () => deleteMail(mail)}>🗑</button></td>
             {:else}
+                <td>
+                    <Checkbox 
+                        checked={checkedMail.has(mail)}    
+                        on:checked={(e) => handleCheck(e,mail)}/>
+                </td>
                 <td><b>Unknown</b></td>
                 <td>--</td>
                 <td>--</td>
                 <td>{mail.resource.modified.toISOString()}</td>
-                <td><button on:click|stopPropagation={() => deleteMail(mail)}>🗑</button></td>
+                <td><button class="delete" on:click|stopPropagation={() => deleteMail(mail)}>🗑</button></td>
             {/if}
         </tr>
     {/await}
   {/each}
   {#if inboxResources.length > maxItems}
         <tr on:click={ () => { maxItems += maxItems }}>
-            <td colspan="5">
+            <td colspan="6">
                 ...older messages...
             </td>
         </tr>
   {/if}
   </tbody>
   </table>
-  {:else}
+{:else}
     (no messages available)
-  {/if}
+{/if}
+
+<style>
+    .list-menu {
+        margin-left: 7px;
+        margin-bottom: 5px;
+    }
+
+    button.delete {
+        width: 30px;
+        height: 30px;
+    }
+</style>
